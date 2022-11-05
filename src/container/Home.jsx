63fc6a3eb43e551338d76cf894e823a0d807afe1
crisnta <torresalvarezcristobal@gmail.com>
2022-11-05 17:09:00 -0300

    pin jsx

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -15,9 +15,9 @@ const Home = () => {
   const scrollRef = useRef(null)
 
   const userInfo = localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
-
+  
   useEffect(() =>{
-    const query = userQuery(userInfo?._id)
+    const query = userQuery(userInfo?.sub)
 
     client.fetch(query).then((data) => {
       setUser(data[0])

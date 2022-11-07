6330d573e8d243936a26a27326557e68650fef6a
crisnta <torresalvarezcristobal@gmail.com>
2022-11-06 21:07:46 -0300

    close

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,37 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import { useParams} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { client } from '../client'
-import { feedQuery, searchQuery } from '../utils/data'
-import MasonryLayout from './MasonryLayout'
-import Spinner from './Spinner'
-
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true)
-  const [pins, setPins] = useState()
-  const { categoryId } = useParams()
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
 
-  useEffect(() =>{
-    if(categoryId){
-      const query = searchQuery(categoryId)
-
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
       client.fetch(query).then((data) => {
-        setPins(data)
-        setLoading(false)
+        setPins(data);
+        setLoading(false);
       });
-    }else{
-      client.fetch(feedQuery).then((data =>{
-        setPins(data)
-        setLoading(false)
-      }))
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+        console.log('data-->', data)
+      });
     }
-  }, [categoryId]) 
-  if(loading) return <Spinner message="We are adding new ideas to your feed!" />
+  }, [categoryId]);
 
+  
+  const ideaName = categoryId || 'new';
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
   return (
-    <div>{pins && <MasonryLayout pins={pins}/>}</div>
-  )
-}
+    <div>
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { RiHomeFill} from 'react-icons/ri';
+import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
-
 import logo from '../assets/logo.png';
+import { categories } from '../utils/data';
 
-const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize'
-const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
 
-const categories = [
-  { name: 'Animals'},
-  { name: 'Wallpapers'},
-  { name: 'Photography'},
-  { name: 'Gaming'},
-  { name: 'Coding'},
-  { name: 'Other'}
-]
-
-const Sidebar = ({ user, closeToggle }) => {
-
-  const handleCloseSidebar = () =>{
-    if(closeToggle) closeToggle(false);
-  }
+const Sidebar = ({ closeToggle, user }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
 
   return (
     <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
@@ -69,7 +59,7 @@ const Sidebar = ({ user, closeToggle }) => {
         </Link>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;

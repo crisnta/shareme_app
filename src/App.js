c0ccb3de8d1d5088accafbd071848ac6ccd95228
crisnta <torresalvarezcristobal@gmail.com>
2022-11-06 22:35:52 -0300

    redirects

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import Home from './container/Home'
 import Login from './components/Login'
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
       <Routes>
